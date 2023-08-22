c03844f5ffefd651266e90f1499a29e5909bd5e7
0xmycf <mycf.mycf.mycf@gmail.com>
2023-08-22 17:56:24 +0200

    prob14 & readme

--- prob14/main.ml
+++ prob14/main.ml
@@ -1,1 +1,26 @@
-let () = assert true
+(*
+   14. Duplicate the elements of a list. (easy)
+   Solution
+
+   # duplicate ["a"; "b"; "c"; "c"; "d"];;
+   - : string list = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"]
+*)
+
+let rec duplicate = function
+  | x :: xs -> x :: x :: duplicate xs
+  | v -> v
+;;
+
+(* Remark: this function is not tail recursive. Can you modify it so it becomes so? *)
+(* Sure! *)
+
+let tailrec_duplicate = 
+  (* let go acc v = v :: v :: acc  *)
+  (* or *)
+  let go acc v = acc @ [v; v] (* (With this its even pointfree!) *)
+  in List.fold_left go []
+
+let ls = [ "a"; "b"; "c"; "c"; "d" ]
+let expected = [ "a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d" ]
+let () = assert (duplicate ls = expected)
+let () = assert (tailrec_duplicate ls = expected)
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 [v2.ocaml.org/...](https://v2.ocaml.org/learn/tutorials/99problems.html)  
-Completed: 1-12
+Completed: 1-14
