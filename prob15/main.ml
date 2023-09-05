362702b49ce94ef0be92fe7f26270decfc671883
0xmycf <mycf.mycf.mycf@gmail.com>
2023-09-05 14:13:27 +0200

    prob15

--- prob15/main.ml
+++ prob15/main.ml
@@ -1,1 +1,20 @@
-let () = assert true
+(*
+   15. Replicate the elements of a list a given number of times. (medium)
+*)
+
+let rec replicate ls n =
+  match ls with
+  | [] -> []
+  | hd :: tl -> List.init n (Fun.const hd) @ replicate tl n
+;;
+
+let input = [ "a"; "b"; "c" ], 3
+let expected = [ "a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c" ]
+let uncurry f (a, b) = f a b
+let () = assert ((uncurry @@ replicate) input = expected)
+
+let () =
+  let ans = (uncurry @@ replicate) input in
+  print_newline ();
+  List.iter (Printf.printf "%s\n") ans
+;;
