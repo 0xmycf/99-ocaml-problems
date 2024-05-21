5c5465f439a93308141acce21aa7824bb433c21a
0xmycf <mycf.mycf.mycf@gmail.com>
2024-05-21 13:16:51 +0200

    prob20

--- prob20/main.ml
+++ prob20/main.ml
@@ -1,1 +1,23 @@
-let () = assert true
+(*
+   20. Remove the K'th element from a list. (easy)
+
+   The first element of the list is numbered 0, the second 1,...
+*)
+
+let remove_at n ls =
+  let aux acc v =
+    let m, ls = acc in
+    if m = 0 then
+      m - 1, ls
+    else
+      m - 1, v :: ls
+  in
+  let _, ret = List.fold_left aux (n, []) ls in
+  List.rev ret
+;;
+
+let () =
+  let res = remove_at 1 [ "a"; "b"; "c"; "d" ] in
+  let list = [ "a"; "c"; "d" ] in
+  assert (list = res)
+;;
