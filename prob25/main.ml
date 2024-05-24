30108c033459562575cc70f4b56c995779d9e55d
0xmycf <mycf.mycf.mycf@gmail.com>
2024-05-25 00:30:13 +0200

    prob25

--- prob25/main.ml
+++ prob25/main.ml
@@ -1,1 +1,32 @@
-let () = assert true
+(*
+   25. Generate a random permutation of the elements of a list. (easy)
+*)
+
+let permutation ls =
+  let len = List.length ls in
+  let gen () =
+    let idx = Random.int len
+    in List.nth ls idx
+  in
+  List.init
+    len
+    (fun _ -> gen ())
+  [@@ocamlformat "disable"]
+
+let test ls tst len =
+  List.length ls = len &&
+  List.fold_left
+    (fun acc v -> acc && List.mem v tst )
+    true
+    ls
+  [@@ocamlformat "disable"]
+
+let () =
+  let ls = [ "a"; "b"; "c"; "d"; "e"; "f" ] in
+  let _res = permutation ls in
+  let _list = [ "a"; "e"; "f"; "b"; "d"; "c" ] in
+  for _ = 0 to 100 do
+    let res = permutation ls in
+    assert (test res ls (List.length ls))
+  done
+;;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 [v2.ocaml.org/...](https://v2.ocaml.org/learn/tutorials/99problems.html)  
-Completed: 0-24
+Completed: 0-25
 
 you can run this with: `dune exec probXX`
