b3a004200cc0e0e69fc1fc47d5559d7c975a4f5e
0xmycf <mycf.mycf.mycf@gmail.com>
2024-05-24 18:28:51 +0200

    prob23

--- prob23/main.ml
+++ prob23/main.ml
@@ -1,1 +1,36 @@
-let () = assert true
+(*
+   23. Extract a given number of randomly selected elements from a list. (medium)
+
+   The selected items shall be returned in a list. We use the Random module but do not initialize it with Random.self_init for reproducibility.
+*)
+
+(** @param n = amount of values to pick from the list *)
+let rand_select xs n =
+  let aux xs =
+    let idx = Random.int (List.length xs - 1) in
+    List.nth xs idx
+  in
+  List.init n (fun _ -> aux xs)
+;;
+
+let test xs ys amount =
+    List.length xs == amount
+  &&
+    List.fold_left
+    (fun acc x -> acc && List.mem x ys)
+    true
+    xs
+  [@@ocamlformat "disable"]
+
+let () =
+  let total = [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h" ] in
+  let res = rand_select total 3 in
+  let _list_example = [ "g"; "d"; "a" ] in
+  assert (test res total 3);
+  for i = 1 to List.length total - 1 do
+    let res = rand_select total i in
+    List.iter (Printf.printf "%s ") res;
+    print_newline ();
+    assert (test res total i)
+  done
+;;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 [v2.ocaml.org/...](https://v2.ocaml.org/learn/tutorials/99problems.html)  
-Completed: 0-22
+Completed: 0-23
 
 you can run this with: `dune exec probXX`
