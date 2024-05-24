a385c23d9834693edd5c4ca6ef7be080328b9b5b
0xmycf <mycf.mycf.mycf@gmail.com>
2024-05-24 18:45:39 +0200

    prob24

--- prob24/main.ml
+++ prob24/main.ml
@@ -1,1 +1,47 @@
-let () = assert true
+(*
+   24. Lotto: Draw N different random numbers from the set 1..M. (easy)
+
+   The selected numbers shall be returned in a list.
+*)
+
+(*
+  imperative functional programming????
+ *)
+let lotto_select select out_of =
+  let arr = Array.make select 0 in
+  let seen = ref ([] : int list) in
+  let upper = out_of + 1 in
+  let draw () =
+    let rand = ref @@ Random.int upper in
+    while List.mem !rand !seen do
+      rand := Random.int upper
+    done;
+    rand
+  in
+  for i = 0 to select - 1 do
+    arr.(i) <- !(draw ())
+  done;
+  Array.to_list arr
+;;
+
+let test thing i j =
+  List.length thing = i
+    &&
+  List.fold_left
+    (fun acc x -> acc && x <= j)
+    true
+    thing
+  [@@ocamlformat "disable"]
+
+let () =
+  let _res = lotto_select 6 49 in
+  let _list = [ 10; 20; 44; 22; 41; 2 ] in
+  for i = 2 to 10 do
+    for j = i to 100_000 do
+      let thing = lotto_select i j in
+      List.iter (Printf.printf "%d ") thing;
+      print_newline ();
+      assert (test thing i j)
+    done
+  done
+;;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 [v2.ocaml.org/...](https://v2.ocaml.org/learn/tutorials/99problems.html)  
-Completed: 0-23
+Completed: 0-24
 
 you can run this with: `dune exec probXX`
